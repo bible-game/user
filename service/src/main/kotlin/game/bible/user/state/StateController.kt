432582486d435bcc66bac7a72659545fc6f783a0
jrsmth <jamesrichardsmith97@gmail.com>
2025-06-25 22:07:41 +0100

    [patch] BLE-123: add CurrentUser obj

--- service/src/main/kotlin/game/bible/user/state/StateController.kt
+++ service/src/main/kotlin/game/bible/user/state/StateController.kt
@@ -1,9 +1,10 @@
 package game.bible.user.state
 
+import game.bible.common.util.security.TokenManager
+import game.bible.user.UserService
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,12 +16,16 @@ private val log = KotlinLogging.logger {}
  */
 @RestController
 @RequestMapping("/state")
-class StateController(private val service: StateService) {
+class StateController(
+    private val service: StateService,
+    private val tokenManager: TokenManager
+) {
 
     /** Returns the game state for a given user */
-    @GetMapping("/game/{userId}")
-    fun getGameState(@PathVariable userId: Long): ResponseEntity<Any> {
+    @GetMapping("/game/")
+    fun getGameState(): ResponseEntity<Any> {
         return try {
+            val userId = tokenManager.get("user", CurrentUser::class.java).userId
             log.info { "Game state request received for user with id [$userId]" }
 
             val state = service.retrieveGameState(userId)
