66b12077b28b488acdaa0f83860f4b7a2893733a
jrsmth <jamesrichardsmith97@gmail.com>
2025-06-26 15:35:40 +0100

    [patch] BLE-123: update security

--- service/src/main/kotlin/game/bible/user/config/Beans.kt
+++ service/src/main/kotlin/game/bible/user/config/Beans.kt
@@ -3,15 +3,11 @@ package game.bible.user.config
 import com.fasterxml.jackson.databind.ObjectMapper
 import game.bible.common.util.security.TokenFilter
 import game.bible.common.util.security.TokenManager
-import game.bible.user.UserRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.authentication.AuthenticationProvider
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import game.bible.config.ReloadableConfig
@@ -27,18 +23,6 @@ import jakarta.servlet.http.HttpServletRequest
     ReloadableConfig::class,
     SecurityConfig::class)
 class Beans {
-
-    @Bean
-    fun userDetailsService(userRepository: UserRepository): UserDetailsService =
-        CustomUserDetailsService(userRepository)
-
-    @Bean
-    fun authenticationProvider(userRepository: UserRepository): AuthenticationProvider =
-        DaoAuthenticationProvider()
-            .also {
-                it.setUserDetailsService(userDetailsService(userRepository))
-                it.setPasswordEncoder(passwordEncoder())
-            }
 
     @Bean
     fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager =
--- service/src/main/kotlin/game/bible/user/config/Security.kt
+++ service/src/main/kotlin/game/bible/user/config/Security.kt
@@ -4,27 +4,24 @@ import game.bible.common.util.security.TokenFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
-import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.DefaultSecurityFilterChain
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 /**
  * Service-level Security Settings
  */
 @Configuration @EnableWebSecurity
 class Security(
-    private val tokenFilter: TokenFilter,
-    private val authProvider: AuthenticationProvider) {
+    private val tokenFilter: TokenFilter) {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): DefaultSecurityFilterChain {
         http
             .csrf { it.disable() }
-            .authenticationProvider(authProvider)
-            .addFilterAfter(tokenFilter, BasicAuthenticationFilter::class.java)
+            .addFilterAfter(tokenFilter, UsernamePasswordAuthenticationFilter::class.java)
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             .authorizeHttpRequests {
                 it
