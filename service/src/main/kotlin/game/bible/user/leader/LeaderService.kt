78d9c5c93d88a3300399a8bf7b7f0c647a52d06d
jrsmth <jamesrichardsmith97@gmail.com>
2025-07-20 18:26:34 +0100

    [patch] BLE-200: leader sorting

--- service/src/main/kotlin/game/bible/user/leader/LeaderService.kt
+++ service/src/main/kotlin/game/bible/user/leader/LeaderService.kt
@@ -14,6 +14,8 @@ class LeaderService(private val repository: UserRepository) {
     fun getLeaders(limit: Int): List<LeaderResponse> {
         return repository.getLeaders(PageRequest.of(0, limit)).map {
             LeaderResponse(it[0] as Long, it[1] as String, it[2] as String, it[3] as Long, it[4] as Long)
+        }.sortedBy {
+            it.gameStars + it.reviewStars
         }
     }
 
