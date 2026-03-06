acbadc1d271af5d1111825d784fab7684f4fa2f0
Hayden <haydeneastwell@proton.me>
2026-03-06 22:12:47 +0000

    [patch] Exclude spring-data-redis dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,9 @@ subprojects {
         implementation(rootProject.libs.bundles.common)
         implementation(rootProject.libs.bundles.config)
         implementation(rootProject.libs.bundles.core)
-        implementation(rootProject.libs.bundles.data)
+        implementation(rootProject.libs.bundles.data) {
+            exclude(module = "spring-data-redis")
+        }
         implementation(rootProject.libs.bundles.database)
         implementation(rootProject.libs.bundles.integration)
         implementation(rootProject.libs.bundles.jwt)
